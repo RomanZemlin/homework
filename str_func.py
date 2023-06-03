dfdcfbf461aa5d2449145be8fc75de3b9f3cf066
RomanZemlin <roman.zemlin@mail.ru>
2023-06-03 21:29:56 +0300

    upper and title word

--- str_func.py
+++ str_func.py
@@ -1,3 +1,8 @@
-def title_word(word):
+def upper_word(word):
     """Делает текст большими буквами"""
+    return word.upper
+
+
+def title_word(word):
+    """Делает первую букву слова большой"""
     return word.title
